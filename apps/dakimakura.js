34a70384e7e2e6ebb99a9c01f3305c71311ad5f5
curus-m <eliminationatonce@gmail.com>
2020-08-14 11:34:33 +0900

    getItem and list

--- apps/dakimakura.js
+++ apps/dakimakura.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 const { Pool } = require('pg')
-const queries = require('./queries.js')
+const queries = require('../config/queries')
+const config = require('../config/config')
 const pool = new Pool()
 const bucketName = "goods-resources";
 const dakimakuraFolder = "resources/dakimakura/";
@@ -13,9 +14,96 @@ const s3Config = {
 const pageItems = 10;
 const s3 = new AWS.S3(s3Config);
 
+const checkData  = function(data) {
+    const errors = [];
+
+    if(!data.name) {
+        errors.push("Name Error");
+    }
+    if (!data.brand) {
+        errors.push("Brand Error");
+    }
+    if (isNaN(data.price)) {
+        errors.push("Name Error");
+    }
+    if (isNaN(data.material)) {
+        errors.push("Material Error");
+    }
+    if (!data.releasedate) {
+        errors.push("Date Error");
+    }
+    console.log("errors:" + errors);
+    return errors.length > 0 ? false : true;
+}
+// change fileName on s3 
+const changeFile = async function (oldFileName) {
+    const date = new Date();
+    let extension = `${oldFileName.split(".")[1]}`
+    let newFileName = `${yyyymmddhhmmss(date)}.${extension}`;
+    const copyParams = {
+        Bucket: bucketName, 
+        Key: `${dakimakuraFolder}${newFileName}`,
+        CopySource:  `${bucketName}/${dakimakuraFolder}${oldFileName}`,
+        ACL: "public-read"
+    }
+    s3.copyObject(copyParams, (err, data) => {
+            if (err) { 
+                console.log(err, err.stack); 
+                Promise.reject(err);
+            }   
+            // an error occurred
+            else {
+                console.log("Copy complete");
+                Promise.resolve(newFileName);
+            }
+    })
+    
+}
+
+const deleteFile = function(fileName) {
+    const deleteParams = {
+        Bucket: bucketName, 
+        Key:  `${dakimakuraFolder}${fileName}`,
+    }
+    
+    s3.deleteObject(deleteParams, (err, data) => {
+        if (err) { 
+            console.log(err, err.stack); // an error occurred
+            throw Error(err);
+        }
+        else {
+            console.log("delete Complete")
+        }
+    });
+}
+const yyyymmddhhmmss = function(date) {
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1); // getMonth() is zero-based
+    var dd  = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    var hh = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    var min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    var ss = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    return "".concat(yyyy).concat(mm).concat(dd).concat(hh).concat(min).concat(ss);
+};
+ 
+
 module.exports = {
     getItem: function(req,res,next) {
-
+        const no = req.params.id;
+    
+        (async (no) => {
+            const client = await pool.connect()
+            const query = queries.getDakiItem
+            const param = [no]
+            try {
+                const result = await client.query(query,param)
+                const data = result.rows[0];
+                console.log(data);
+                res.json(data);
+            } finally {
+                client.release()
+            }
+        })(no).catch(err => console.log(err.stack));
     },
     getList: function(req,res,next) {
         // console.log('Request Type:', req.method);
@@ -33,7 +121,7 @@ module.exports = {
             categoryStart = category;
             categoryEnd = category;
         }
-        console.log(`page: ${pages} , query: ${searchQuery}, category: ${category}`);
+        console.log(`page: ${pages} , query: ${req.params.query}, category: ${category}`);
         let offset = pageItems * (pages-1);
         (async () => {
             const client = await pool.connect();
@@ -49,14 +137,14 @@ module.exports = {
                     dakimakuras: dakiList.rows,
                     totalPages: count
                 }
-                callback(null, createResponse(200, data))
+                res.json(data);
             } finally {
                 client.release()
             }
         })().catch(err => console.log(err.stack))
 
 
-        res.send(data);
+      
     }
 
 };
--- routes/dakimakura.js
+++ routes/dakimakura.js
@@ -9,4 +9,11 @@ router.get('/', function(req, res, next) {
     next();
 }, dakimakura.getList);
 
+router.get('/:id', function (req, res, next) {
+    // if the user ID is 0, skip to the next router
+    if (req.params.id == 0) next('route');
+    // otherwise pass control to the next middleware function in this stack
+    else next(); //
+  }, dakimakura.getItem);
+  
 module.exports = router;
